cbd054e13dc5df6ae187bb09ae5704f46c0006bb
Cyborg-Mcloud <uabashidze@gmail.com>
2018-06-25 14:54:06 +0400

    რე
    ეე

--- www/js/maps.js
+++ www/js/maps.js
@@ -482,7 +482,7 @@ function calcRoute(from_loc, to_loc, directionsService, directionsDisplay)
 			var	tanxa=datvale_pussy(metrebi_real);
 			
 			document.getElementById("dirinfo").innerHTML='<div class="container-fluid py-2" style="border-bottom: 1px solid red"><div class="text-center">მანძილი: '+metrebi_real+'კმ</div></div>';
-			document.getElementById("dirinfo").innerHTML+='<div class="container-fluid py-2"><div class="text-center"> სავარაუდო ფასი: '+parseInt(tanxa) +" ლარი</div></div>";
+			document.getElementById("dirinfo").innerHTML+='<div class="container-fluid py-2"><div class="text-center"> სავარაუდო ფასი: '+parseInt(tanxa) +"-"+parseInt(tanxa+1)+"₾</div></div>";
 			document.getElementById("dirinfo").style.display="block";
 			document.getElementById("call_div").style.bottom="210px";
 			document.getElementById("car_choose").style.bottom="50px";
